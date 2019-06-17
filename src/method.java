e39219d74538f98e8d844133c4008824a9b924c3
xiaoba <xiaoba@qq.com>
2019-06-17 19:20:27 +0800

    添加2

--- src/method.java
+++ src/method.java
@@ -1,5 +1,10 @@
 public class method {
     public static void main(String[] args) {
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
+        System.out.println("hello.....");
 
     }
 }
